ebcfb9942f7c9de0dc5ae62e4c143472846312d1
alvaro <alvaroabascar@gmail.com>
2015-10-10 23:09:15 +0200

    patients service

--- src/app/dr-patients/dr-patients.controller.js
+++ src/app/dr-patients/dr-patients.controller.js
@@ -6,15 +6,9 @@
     .controller('DrPatientsController', DrPatientsController);
 
   /** @ngInject */
-  function DrPatientsController($timeout, $scope, $location, webDevTec, toastr) {
+  function DrPatientsController($timeout, $scope, $location, webDevTec, toastr, patientsService) {
     var vm = this;
-    $scope.patients = [{name: "Arushi", female: true, cancer: "Left eye cancer"},
-                       {name: "Gabi", female: false, cancer: "Pancreas cancer"},
-                       {name: "Álvaro", female: false, cancer: "Right ear cancer"},
-                       {name: "Morten", female: false, cancer: "Nail cancer"},
-                       {name: "John", female: false, cancer: "Happy cancer"},
-                       {name: "Yenyin", female: true, cancer: "Sad cancer"},
-                       {name: "Jui", female: true, cancer: "Toe cancer"}];
+    $scope.patients = patientsService;
     $scope.go = function ( path ) {
         $location.path(path);
     }
---
+++ src/app/dr-patients/dr-patients.service.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('opencancerWeb')
+    .factory('patientsService', function($http) {
+        // function body
+        var patients = [{name: "Arushi", female: true, cancer: "Left eye cancer"},
+                    {name: "Gabi", female: false, cancer: "Pancreas cancer"},
+                    {name: "Álvaro", female: false, cancer: "Right ear cancer"},
+                    {name: "Morten", female: false, cancer: "Nail cancer"},
+                    {name: "John", female: false, cancer: "Happy cancer"},
+                    {name: "Yenyin", female: true, cancer: "Sad cancer"},
+                    {name: "Jui", female: true, cancer: "Toe cancer"}
+                   ];
+        return patients;
+    });
+ }
+)();
