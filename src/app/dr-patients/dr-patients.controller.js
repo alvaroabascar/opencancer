e89819b11419e820fdcacec6168245c05f0a4169
alvaro <alvaroabascar@gmail.com>
2015-10-10 22:33:34 +0200

    :cat:

--- src/app/dr-patients/dr-patients.controller.js
+++ src/app/dr-patients/dr-patients.controller.js
@@ -3,11 +3,18 @@
 
   angular
     .module('opencancerWeb')
-    .controller('DrPatientsControllers', DrPatientsControllers);
+    .controller('DrPatientsController', DrPatientsController);
 
   /** @ngInject */
-  function DrPatientsControllers($timeout, $scope, $location, webDevTec, toastr) {
+  function DrPatientsController($timeout, $scope, $location, webDevTec, toastr) {
     var vm = this;
+    $scope.patients = [{name: "Arushi", female: true, cancer: "Left eye cancer"},
+                       {name: "Gabi", female: false, cancer: "Pancreas cancer"},
+                       {name: "Álvaro", female: false, cancer: "Right ear cancer"},
+                       {name: "Morten", female: false, cancer: "Nail cancer"},
+                       {name: "John", female: false, cancer: "Happy cancer"},
+                       {name: "Yenyin", female: true, cancer: "Sad cancer"},
+                       {name: "Jui", female: true, cancer: "Toe cancer"}];
     $scope.go = function ( path ) {
         $location.path(path);
     }
